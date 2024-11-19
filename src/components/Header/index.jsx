964e69982f435b55b5ce3016f33cdfb1b1f01a26
mehedirana <mdmehedirana@gmail.com>
2024-11-19 15:01:08 +0600

    nav bar and side bar fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import DropdownNotification from './DropdownNotification';
 import DropdownMessage from './DropdownMessage';
 import DropdownUser from './DropdownUser';
 
-const Header = (sidebarOpen, setSidebarOpen) => {
+const Header = ({ sidebarOpen, setSidebarOpen }) => {
     return (
         <div className=' sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none'>
             <div className='flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11'>
@@ -22,25 +22,25 @@ const Header = (sidebarOpen, setSidebarOpen) => {
                         <span className="relative block h-5.5 w-5.5 cursor-pointer">
                             <span className="du-block absolute right-0 h-full w-full">
                                 <span
-                                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${sidebarOpen && '!w-full delay-300'
+                                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${!sidebarOpen && '!w-full delay-300'
                                         }`}
                                 ></span>
                                 <span
-                                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${sidebarOpen && 'delay-400 !w-full'
+                                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${!sidebarOpen && 'delay-400 !w-full'
                                         }`}
                                 ></span>
                                 <span
-                                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${sidebarOpen && '!w-full delay-500'
+                                    className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${!sidebarOpen && '!w-full delay-500'
                                         }`}
                                 ></span>
                             </span>
                             <span className="absolute right-0 h-full w-full rotate-45">
                                 <span
-                                    className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${sidebarOpen && '!h-0 !delay-[0]'
+                                    className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${!sidebarOpen && '!h-0 !delay-[0]'
                                         }`}
                                 ></span>
                                 <span
-                                    className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${sidebarOpen && '!h-0 !delay-200'
+                                    className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${!sidebarOpen && '!h-0 !delay-200'
                                         }`}
                                 ></span>
                             </span>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { NavLink, useLocation } from 'react-router-dom';
 import Logo from '../../images/logo/logo.avif';
 import SidebarLinkGroup from './SiderbarLinkGroup';
@@ -15,8 +15,45 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
         storedSidebarExpanded === null ? false : storedSidebarExpanded === 'true'
     );
 
+    // close on click outside
+    useEffect(() => {
+        const clickHandler = ({ target }) => {
+            if (!sidebar.current || !trigger.current) return;
+            if (
+                !sidebarOpen ||
+                sidebar.current.contains(target) ||
+                trigger.current.contains(target)
+            )
+                return;
+            setSidebarOpen(false);
+        };
+        document.addEventListener('click', clickHandler);
+        return () => document.removeEventListener('click', clickHandler);
+    });
+
+    // close if the esc key is pressed
+    useEffect(() => {
+        const keyHandler = ({ keyCode }) => {
+            if (!sidebarOpen || keyCode !== 27) return;
+            setSidebarOpen(false);
+        };
+        document.addEventListener('keydown', keyHandler);
+        return () => document.removeEventListener('keydown', keyHandler);
+    });
+
+    useEffect(() => {
+        localStorage.setItem('sidebar-expanded', sidebarExpanded.toString());
+        if (sidebarExpanded) {
+            document.querySelector('body')?.classList.add('sidebar-expanded');
+        } else {
+            document.querySelector('body')?.classList.remove('sidebar-expanded');
+        }
+    }, [sidebarExpanded]);
+
     return (
-        <aside className={`absolute left-0 top-0 z-9999 flex flex-col h-screen w-72 overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0
+        <aside
+            ref={sidebar}
+            className={`absolute left-0 top-0 z-9999 flex flex-col h-screen w-72 overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0
             ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
             `}>
             <div className='flex items-center justify-center gap-2 px-6 py-5.5 lg:py-6.5'>
@@ -67,7 +104,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                                             <NavLink
                                                 to="#"
                                                 className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${(pathname === '/' ||
-                                                        pathname.includes('dashboard')) &&
+                                                    pathname.includes('dashboard')) &&
                                                     'bg-graydark dark:bg-meta-4'
                                                     }`}
                                                 onClick={(e) => {
@@ -213,7 +250,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                                             <NavLink
                                                 to="#"
                                                 className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${(pathname === '/forms' ||
-                                                        pathname.includes('forms')) &&
+                                                    pathname.includes('forms')) &&
                                                     'bg-graydark dark:bg-meta-4'
                                                     }`}
                                                 onClick={(e) => {
@@ -387,8 +424,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                             {/* <!-- Menu Item Settings --> */}
                         </ul>
                     </div>
-                    {/* others  */}
-                    <div>others</div>
                 </nav>
             </div>
         </aside >
